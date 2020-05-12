e262dcc2cc9b97bb0324e41c89b51a7d25a843cb
Salim Romadhon <slmrmdhn@gmail.com>
2020-05-12 14:57:56 +0700

    Add about page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ route('about') }}">About</a>
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
@@ -81,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ $title ?? 'Laravel' }}
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/about', function () {
+    $title = 'Salim Romadhon';
+    return view('welcome', compact('title'));
+})->name('about');
